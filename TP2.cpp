c97cd24d96f4fcc31722e532322e832ca5bb3f67
MatthieuGomes <matthieu.gomes@ensea.fr>
2025-10-22 13:50:51 +0200

    moving files from TP1 + adding new ressources

---
+++ TP2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+
+int main(int argc, char* argv[])
+{
+    std::cout << "Hello, World!" << std::endl;
+    return EXIT_SUCCESS;
+}
